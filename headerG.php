ba023ce56243bb78172ac49d01aa0caaef10b1af
VincentGouygou <34983460+VincentGouygou@users.noreply.github.com>
2025-03-26 13:29:13 +0100

    DP
    Nettoyés expliqués

--- headerG.php
+++ headerG.php
@@ -5,10 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aire naturelle "Les Grands Chênes"</title>
     <link rel="stylesheet" href="style1.css">
-     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js">
-        
-     </script>
- 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js">
+    </script>
 </head>
-<body class="   ">
+<body class=" ">
      
